305e70410efc1e09f599bf67c5b82fab383b1998
anil <anilkumar.kammalapalli@tarento.com>
2023-03-17 17:54:33 +0530

    worked on term associations for workflow

--- platform-modules/actors/src/main/java/org/sunbird/learning/framework/FrameworkHierarchy.java
+++ platform-modules/actors/src/main/java/org/sunbird/learning/framework/FrameworkHierarchy.java
@@ -22,12 +22,7 @@ import org.sunbird.graph.model.cache.CategoryCache;
 import org.sunbird.graph.model.node.DefinitionDTO;
 import org.sunbird.learning.hierarchy.store.HierarchyStore;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author pradyumna
